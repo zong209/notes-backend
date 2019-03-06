78ea6cfa7c10726bf3db2f9099bda7756d6eb52e
gz <gz@gzdeMacBook-Pro.local>
2019-03-06 22:45:33 +0800

    change db address

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // host: 'htdq.ansi.space', // Hostname of IoTgo platform
     db: {
-        uri: 'mongodb://127.0.0.1:27017/NOTES', // MongoDB database address
+        uri: 'mongodb://134.175.55.42:27017/NOTES', // MongoDB database address
     },
     page:{
         limit:5,
